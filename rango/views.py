451137c04ff1d3f44afa289e573a27117338a410
“Temi <2619191a@student.gla.ac.uk>
2023-02-09 22:12:04 +0000

    Chapter 4

--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    context_dict={'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+    #return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage': 'This tutorial has been put together by Temi Adeola'}
+    return render(request, 'rango/about.html', context = context_dict)
+    #return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
